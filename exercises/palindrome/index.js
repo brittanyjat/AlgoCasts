8602cc4853f68d7dd458ab8ec02c9d67d2a83013
Brittany Jones <brittanyjones@PC-LW31117.overstock.com>
2019-01-09 08:33:42 -0700

    palindrome, reverseint, reversestring

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,15 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// function palindrome(str) {
+//     let resversed = str.split('').reduce((rev, char)=> char + rev, '');
+//     return str === resversed;
+// }
+
+function palindrome(str) {
+    return str.split('').every((char, i) => {
+      return char === str[str.length - i - 1]
+    });
+  }
 
 module.exports = palindrome;
--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,26 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// function reverse(str) {
+//     return str.split('').reverse().join('')
+// }
+
+// function reverse(str) {
+//     let temp = str.split('');
+//     let reversed = '';
+//     for (let char of str){
+//       reversed = char + reversed
+//     }
+//     return reversed;
+//   }
+
+function reverse(str) {
+    debugger;
+    return str.split('').reduce((result, char) =>
+        char + result
+        , '');
+}
+
+reverse('asdf');
 
 module.exports = reverse;
