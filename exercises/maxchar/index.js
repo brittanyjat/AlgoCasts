6a33fe220c9e9788c339c2cfe26a6cc11b4d3391
Brittany Jones <brittanyjones@PC-LW31117.overstock.com>
2019-01-09 09:53:03 -0700

    maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let maxCount = 0;
+    let maxChar = '';
+    let map = mapper(str);
+    for (let char in map){
+      if (map[char] > maxCount) {
+        maxCount = map[char];
+        maxChar = char;
+      }
+    }
+    return maxChar;
+  }
+  
+  function mapper(str){
+    let obj = {};
+    for (let char of str ){
+      obj[char] = obj[char] + 1 || 1;
+    }
+    return obj;
+  }
 
 module.exports = maxChar;
